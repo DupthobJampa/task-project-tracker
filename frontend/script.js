40ce26e2aeda3bffad8115847bb868fa728c7c4c
Dupthob Jampa Goenpo Wangdi <z5633193@ad.unsw.edu.au>
2026-09-19 01:20:38 +1000

    Load projects and tasks dynamically

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,7 @@
 const taskList = document.querySelector('.task-list');
 const filterButtons = document.querySelectorAll('.filter-button');
+const projectList = document.querySelector('.project-list');
+const projectTitle = document.querySelector('.task-header h2');
 
 function formatLabel(value) {
     return value
@@ -7,9 +9,46 @@ function formatLabel(value) {
         .replace(/\b\w/g, (letter) => letter.toUpperCase());
 }
 
-async function loadTasks() {
-    const response = await fetch('http://127.0.0.1:8000/tasks?project_id=4');
+async function loadProjects() {
+    const response = await fetch('http://127.0.0.1:8000/projects');
+    const projects = await response.json();
+
+    projects.forEach((project) => {
+        const item = document.createElement('li');
+        item.textContent = project.name;
+        item.dataset.projectId = project.id;
+
+        item.addEventListener('click', () => {
+            document.querySelectorAll('.project-list li').forEach((item) => {
+                item.classList.remove('active');
+            });
+
+            item.classList.add('active');
+
+            projectTitle.textContent = project.name;
+            loadTasks(project.id);
+        });
+        projectList.appendChild(item);
+    });
+
+    const firstProject = projectList.querySelector('li');
+
+    if (firstProject) {
+        firstProject.click();
+    }
+}
+
+async function loadTasks(projectId) {
+
+    const response = await fetch(
+
+        `http://127.0.0.1:8000/tasks?project_id=${projectId}`
+
+    );
+
     const tasks = await response.json();
+
+    taskList.innerHTML = '';
 
     tasks.forEach((task) => {
         const article = document.createElement('article');
@@ -50,4 +89,4 @@ filterButtons.forEach((button) => {
     });
 });
 
-loadTasks();
+loadProjects();
